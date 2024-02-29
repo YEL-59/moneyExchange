8f8ddfc84acca545ce2da099145e12d631bce0bb
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-03-01 00:01:07 +0600

    update design in home page

--- src/Pages/Home/HomeComponents/Prestige_Benifit/Prestige_Benifit.jsx
+++ src/Pages/Home/HomeComponents/Prestige_Benifit/Prestige_Benifit.jsx
@@ -1,45 +1,73 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
+import { TiTickOutline } from "react-icons/ti";
 const Prestige_Benifit = () => {
+  const items = [
+    {
+      title: "Easy Transaction",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit eiusmod tempor incididunt"
+    },
+    {
+      title: "Guaranteed Security like no other",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit eiusmod tempor incididunt"
+    },
+    {
+      title: "Innovation",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit eiusmod tempor incididunt"
+    }
+  ];
   return (
     <>
       <section class="overflow-hidden bg-white py-8 sm:py-16">
-        <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div class="lg:pr-8 lg:pt-4">
-              <div class="lg:max-w-lg">
+        <div class="container mx-auto ">
+          <div class="mx-auto grid  grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2  ">
+            <motion.img initial={{ x: "-100vw", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 0.5 }} src="/src/assets/Group2.png" alt="Product screenshot" class="w-full h-auto flex justify-center items-center" />
+            <div class="lg:pr-8 l">
 
-                <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Prestige Benefits</p>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet, consectetur dipiscing elit eius mod tempor incididunt ut labore Lorem ipsum dolor sit amet, consectetur dipiscing elit eius mod tempor incididunt ut labore
-                </p>
-                <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
-                  <div class="relative pl-9">
-                    <dt class="inline font-semibold text-gray-900"><img src="/src/assets/Animationbest.gif" alt="" class="absolute left-[-25px]   h-20 w-20 " />
-                    </dt>
-                    <dd class="inline"> <h6 class="mb-2 font-semibold leading-5">Easy Transaction</h6>
-                      <p class="mb-3 text-sm text-gray-900">
-                        Lorem ipsum dolor sit amet, consectetur dipiscing elit eiusmod tempor incididunt
-                      </p>
-                    </dd>
-                  </div>
-                  <div class="relative pl-9">
-                    <dt class="inline font-semibold text-gray-900">
-                      <img src="/src/assets/Animationbest.gif" alt="" class="absolute left-[-25px]  h-20 w-20 " />
+              <div class="">
+                <motion.p
+                  initial={{ x: "-100vw", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ ease: "easeInOut", duration: 1 }}
+                  className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl text-center lg:text-start"
+                >
+                  Prestige Benefits
+                </motion.p>
+                <motion.p
+                  initial={{ x: "-100vw", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ ease: "easeInOut", duration: 1, delay: 0.5 }}
+                  className="mt-6 text-lg leading-8 text-gray-600  text-center lg:text-start"
+                >
+                  Lorem ipsum dolor sit amet, consectetur dipiscing elit eius mod tempor incididunt ut labore Lorem ipsum dolor
+                  sit amet, consectetur dipiscing elit eius mod tempor incididunt ut labore
+                </motion.p>
 
-                    </dt>
-                    <dd class="inline"> <h6 class="mb-2 font-semibold leading-5">Guaranteed Security like no other</h6>
-                      <p class="mb-3 text-sm text-gray-900">
-                        Lorem ipsum dolor sit amet, consectetur dipiscing elit eiusmod tempor incididunt
-                      </p></dd>
-                  </div>
-                  <div class="relative pl-9">
-                    <dt class="inline font-semibold text-gray-900"><img src="/src/assets/Animationbest.gif" alt="" class="absolute left-[-25px]  h-20 w-20 " /></dt>
-                    <dd class="inline"> <h6 class="mb-2 font-semibold leading-5">Innovation</h6>
-                      <p class="mb-3 text-sm text-gray-900">
-                        Lorem ipsum dolor sit amet, consectetur dipiscing elit eiusmod tempor incididunt
-                      </p>
-                    </dd>
-                  </div>
+
+                <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                  {items.map((item, index) => (
+                    <div key={index} className="relative flex items-center gap-5 pl-4">
+                      <motion.dt initial={{ x: "-100vw", opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ ease: "easeInOut", duration: 1 }} className="inline font-semibold text-gray-900">
+
+                        <span className=" grid h-10 w-10 place-items-center rounded-full bg-[#F0FFF4] border-2 relative overflow-hidden transition-bg duration-500 hover:bg-green-500 shadow-lg">
+                          <TiTickOutline className="h-8 w-8 text-green-400 hover:text-white absolute top-0 left-0 right-0 bottom-0 m-auto" />
+                          <span className="bg-gradient-to-t from-green-100 to-green-500 absolute bottom-0 left-0 right-0 top-full"></span>
+                        </span>
+                      </motion.dt>
+                      <dd className="inline">
+                        <motion.h6 initial={{ x: "-100vw", opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{ ease: "easeInOut", duration: 1 }} className="mb-2 font-semibold leading-5 text-black">{item.title}</motion.h6>
+                        <motion.p initial={{ x: "-100vw", opacity: 0 }}
+                          animate={{ x: 0, opacity: 1 }}
+                          transition={{ ease: "easeInOut", duration: 1 }} className="mb-3 text-sm text-gray-900">{item.description}</motion.p>
+                      </dd>
+                    </div>
+                  ))}
                 </dl>
               </div>
               {/* <div class="mt-10 flex items-center gap-x-6">
@@ -52,7 +80,7 @@ const Prestige_Benifit = () => {
                 </a>
               </div> */}
             </div>
-            <img src="/src/assets/Group2.png" alt="Product screenshot" class="w-full h-full" />
+
           </div>
         </div>
       </section>
